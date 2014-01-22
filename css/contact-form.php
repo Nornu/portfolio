956cde048b9fdb62da26f70ceff7780e02d14da6
Nornu <j.cheron@outlook.be>
2014-01-22 17:10:51 +0100

    correctif des bugs de layout

---
+++ css/contact-form.php
@@ -0,0 +1,139 @@
+<?php
+/*
+Template Name: Contact Form
+*/
+?>
+
+<?php 
+//If the form is submitted
+if(isset($_POST['submitted'])) {
+
+    //Check to see if the honeypot captcha field was filled in
+    if(trim($_POST['checking']) !== '') {
+        $captchaError = true;
+    } else {
+    
+        //Check to make sure that the name field is not empty
+        if(trim($_POST['contactName']) === '') {
+            $nameError = 'Indiquez votre nom.';
+            $hasError = true;
+        } else {
+            $name = trim($_POST['contactName']);
+        }
+        
+        //Check to make sure sure that a valid email address is submitted
+        if(trim($_POST['email']) === '')  {
+            $emailError = 'Indiquez une adresse e-mail valide.';
+            $hasError = true;
+        } else if (!eregi("^[A-Z0-9._%-]+@[A-Z0-9._%-]+\.[A-Z]{2,4}$", trim($_POST['email']))) {
+            $emailError = 'Adresse e-mail invalide.';
+            $hasError = true;
+        } else {
+            $email = trim($_POST['email']);
+        }
+            
+        //Check to make sure comments were entered  
+        if(trim($_POST['comments']) === '') {
+            $commentError = 'Entrez votre message.';
+            $hasError = true;
+        } else {
+            if(function_exists('stripslashes')) {
+                $comments = stripslashes(trim($_POST['comments']));
+            } else {
+                $comments = trim($_POST['comments']);
+            }
+        }
+            
+        //If there is no error, send the email
+        if(!isset($hasError)) {
+
+            $emailTo = 'portfolio@jcheron.be';
+            $subject = 'Formulaire de contact de '.$name;
+            $sendCopy = trim($_POST['sendCopy']);
+            $body = "Name: $name \n\nEmail: $email \n\nComments: $comments";
+            $headers = 'De : mon site <'.$emailTo.'>' . "\r\n" . 'R&eacute;pondre &agrave; : ' . $email;
+            //ini_set('SMTP','smtp.jcheron.be');
+            mail($emailTo, $subject, $body, $headers);
+
+            if($sendCopy == true) {
+                $subject = 'Formulaire de contact';
+                $headers = 'De : <noreply@somedomain.com>';
+                mail($email, $subject, $body, $headers);
+            }
+
+            $emailSent = true;
+
+        }
+    }
+} ?>
+
+<?php get_header();?>
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); $sSpeed = array('slower','slow','quick','quicker');?>
+<section class="moi">
+    <h2>Contacter Julien Cheron</h2>
+    <div class="introduction">
+        <p><strong>Me Contacter</strong> N'h&eacute;sitez pas &agrave; remplir le formulaire pour me contacter.</p>
+    </div><!--
+    --><div class="lien load_quicker" id="contact">
+        <a href="<?php
+        $page = get_page_by_title( 'Contact' );
+        $pageID = $page->ID;
+        echo get_page_link( $pageID ); ?>">Contact</a>
+    </div>
+    <div class="contenu blog full_width">
+        <div class="cadre cadre_moi full_height load_<?php $rand_key = array_rand($sSpeed,1); $speed = $sSpeed[$rand_key]; echo $speed ?>">
+
+<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/scripts/contact-form.js"></script>
+
+
+
+<?php if(isset($emailSent) && $emailSent == true) { ?>
+
+    <div class="thanks">
+        <h1>Merci, <?=$name;?></h1>
+        <p>Votre e-mail a &eacute;t&eacute; envoy&eacute; avec succ&egrave;s. Vous recevrez une r&eacute;ponse sous peu.</p>
+    </div>
+
+<?php } else { ?>
+        
+        <?php if(isset($hasError) || isset($captchaError)) { ?>
+            <p class="error">Une erreur est survenue lors de l'envoi du formulaire.</p>
+        <?php } ?>
+    
+        <form action="<?php the_permalink(); ?>" id="contactForm" method="post">
+    
+            <ol class="forms">
+                <li><label for="contactName">Nom</label>
+                    <input type="text" name="contactName" id="contactName" value="<?php if(isset($_POST['contactName'])) echo $_POST['contactName'];?>" class="requiredField" />
+                    <?php if($nameError != '') { ?>
+                        <span class="error"><?=$nameError;?></span> 
+                    <?php } ?>
+                </li>
+                
+                <li><label for="email">E-mail</label>
+                    <input type="text" name="email" id="email" value="<?php if(isset($_POST['email']))  echo $_POST['email'];?>" class="requiredField email" />
+                    <?php if($emailError != '') { ?>
+                        <span class="error"><?=$emailError;?></span>
+                    <?php } ?>
+                </li>
+                
+                <li class="textarea"><label for="commentsText">Message</label>
+                    <textarea name="comments" id="commentsText" rows="20" cols="30" class="requiredField"><?php if(isset($_POST['comments'])) { if(function_exists('stripslashes')) { echo stripslashes($_POST['comments']); } else { echo $_POST['comments']; } } ?></textarea>
+                    <?php if($commentError != '') { ?>
+                        <span class="error"><?=$commentError;?></span> 
+                    <?php } ?>
+                </li>
+                <li class="inline"><input type="checkbox" name="sendCopy" id="sendCopy" value="true"<?php if(isset($_POST['sendCopy']) && $_POST['sendCopy'] == true) echo ' checked="checked"'; ?> /><label for="sendCopy">Recevoir une copie du message</label></li>
+                <li class="screenReader"><label for="checking" class="screenReader">Pour envoyer ce formulaire, ne saisissez RIEN dans ce champ</label><input type="text" name="checking" id="checking" class="screenReader" value="<?php if(isset($_POST['checking']))  echo $_POST['checking'];?>" /></li>
+                <li class="buttons"><input type="hidden" name="submitted" id="submitted" value="true" /><button type="submit">Envoyer</button></li>
+            </ol>
+        </form>
+<?php } ?>
+
+</div>
+    </div>
+</section>
+<?php endwhile;endif;
+$location='contact';
+get_template_part('menu');
+get_footer(); ?>
